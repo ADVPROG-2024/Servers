54e7519835bc6221acb5a137d833379df9518b86
Leffy02 <pietro.gasparini@studenti.unitn.it>
2025-01-27 00:25:23 +0100

    fixato errori di compilazione e implementato serialize per ServerMessage

--- src/main.rs
+++ src/main.rs
@@ -10,25 +10,41 @@ use wg_2024::packet::{FloodRequest, NodeType, Packet, PacketType};
 use std::fs::File;
 use std::thread;
 use std::time::Duration;
+use serde::ser::SerializeStruct;
 // use eframe::egui;
 // use log::LevelFilter;
 // use simplelog::{ConfigBuilder, WriteLogger};
 
+#[derive(Serialize)]
 enum ServerMessageType {
-    RegisteredClient(Vec<Client>),
+    RegisteredClient(Vec<NodeId>),
     Text(String, NodeId),
 }
 
-#[derive(Serialize)]
-struct  ServerMessage {
+struct ServerMessage {
     message: ServerMessageType
 }
 
-#[derive(Serialize)]
 struct CommunicationServer {
     server: Server,
-    registered_client: Vec<Client>,
+    registered_client: Vec<NodeId>,
 }
+
+impl Serialize for ServerMessage {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        use serde::ser::SerializeStruct;
+
+        // Creiamo una struttura serializzata con un singolo campo "message"
+        let mut state = serializer.serialize_struct("ServerMessage", 1)?;
+        state.serialize_field("message", &self.message)?;
+        state.end()
+    }
+}
+
+
 
 impl CommunicationServer {
     fn new() -> CommunicationServer {
@@ -45,7 +61,7 @@ impl CommunicationServer {
         self.server.connected_drone_ids.push(client_id.clone());
     }
 
-    fn find_path(&self, client_id: NodeId) -> Vec<Vec<(NodeId, NodeType)>> {
+    fn find_path(&self, client_id: &NodeId) -> Vec<NodeId> {
         // Creazione dei canali per ciascun neighbour
         let mut senders = HashMap::new();
         let mut receivers = Vec::new(); // Per simulare la ricezione nei vicini
@@ -85,16 +101,7 @@ impl CommunicationServer {
             match receiver.recv() {
                 Ok(received_packet) => {
                     if let PacketType::FloodResponse(response) = received_packet.pack_type {
-                        println!(
-                            "FloodResponse received from neighbour {}: {:?}",
-                            neighbour_id, response
-                        );
                         responses.push((neighbour_id, response));
-                    } else {
-                        println!(
-                            "Unexpected packet type received from neighbour {}",
-                            neighbour_id
-                        );
                     }
                 }
                 Err(_) => {
@@ -109,21 +116,16 @@ impl CommunicationServer {
         if responses.is_empty() {
             println!("No valid FloodResponses received.");
         } else {
-            println!("Processed {} FloodResponses:", responses.len());
-            for (neighbour_id, response) in responses {
-                println!(
-                    "Neighbour {} responded with FloodResponse: {:?}",
-                    neighbour_id, response
-                );
+            for (_, response) in responses {
                 paths.push(response.path_trace);
             }
         }
 
-        let mut possible_neighbours = Vec::new();
+        let mut possible_neighbours:Vec<Vec<NodeId>> = Vec::new();
 
         for path in paths.iter() {
-            if path.iter().any(|x| x == *client_id) {
-                possible_neighbours.push(path.clone());
+            if path.iter().any(|x| x.0 == *client_id) {
+                possible_neighbours.push(path.iter().map(|node| node.0.clone()).collect::<Vec<_>>());
             }
         }
 
@@ -132,7 +134,7 @@ impl CommunicationServer {
         best_path
     }
 
-    fn send_register_drone(&mut self, client_id: NodeId) { // TESTARE!!!!
+    fn send_register_drone(&mut self, client_id: &NodeId) { // TESTARE!!!!
         let hops = self.find_path(client_id);
         let neighbour_id = hops.first().unwrap();
         // Creazione del canale per il neighbor
@@ -146,14 +148,13 @@ impl CommunicationServer {
             message: ServerMessageType::RegisteredClient(self.registered_client.clone()),
         };
 
-        let packets = fragment_message(&data, hops, 1);
+        let packets = fragment_message(&data, hops.clone(), 1);
 
         for mut packet in packets {
             // Invia il pacchetto al neighbour utilizzando il suo NodeId
             if let Some(sender) = senders.get(&neighbour_id) {
                 packet.routing_header.hop_index = 1;
                 sender.send(packet).expect("Errore durante l'invio del pacchetto al neighbour.");
-                println!("Pacchetto inviato al neighbour con NodeId {}", neighbour_id);
             } else {
                 println!("Errore: Neighbour con NodeId {} non trovato!", neighbour_id);
             }
@@ -172,7 +173,7 @@ impl CommunicationServer {
     }
 
     fn forward_message(&mut self, client_id: NodeId, message: ServerMessage) { //non so se ha senso far passare l'intero messaggio oppure il singolo pacchetto
-        let hops = self.find_path(client_id);
+        let hops = self.find_path(&client_id);
         let neighbour_id = hops.first().unwrap();
         // Creazione del canale per il neighbor
         let (neighbor_send, neighbor_recv) = crossbeam_channel::unbounded::<Packet>();
@@ -181,7 +182,7 @@ impl CommunicationServer {
         let mut senders = HashMap::new();
         senders.insert(neighbour_id, neighbor_send.clone());
 
-        let packets = fragment_message(&message, hops, 1);
+        let packets = fragment_message(&message, hops.clone(), 1);
 
         for packet in packets {
             // Invia il pacchetto al neighbour utilizzando il suo NodeId
@@ -195,10 +196,10 @@ impl CommunicationServer {
     }
 }
 
-fn compute_best_path(paths: &Vec<Vec<(NodeId, NodeType)>>, p1: NodeId, p2: NodeId) -> Vec<Vec<(NodeId, NodeType)>> {
+fn compute_best_path(paths: &Vec<Vec<NodeId>>, p1: NodeId, p2: &NodeId) -> Vec<NodeId> {
     todo!()
 }
 
 fn main() {
-
+    println!("ciao fra");
 }
