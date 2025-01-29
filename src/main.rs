033277efa3b9a979377266b4e106d9bc2ea8bb5f
PietroGaspa <136386320+Leffy02@users.noreply.github.com>
2025-01-29 18:28:48 +0100

    ordinato il main

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,12 @@
 mod communication_server;
 mod content_server;
 
-use std::collections::{HashMap, HashSet};
 use std::fmt::Display;
-use crossbeam_channel::{select, select_biased, unbounded, Receiver, Sender};
-use dronegowski_utils::functions::{assembler, deserialize_message, fragment_message, fragmenter};
-use dronegowski_utils::hosts::{ServerType, ServerCommand, ServerEvent, TestMessage, ClientMessages, ServerMessages, TextServer};
+use dronegowski_utils::hosts::{ServerMessages};
 use dronegowski_utils::functions::generate_unique_id;
 use wg_2024::network::{NodeId};
-use wg_2024::packet::{FloodRequest, Fragment, NodeType, Packet, PacketType};
-use std::fs::File;
+use wg_2024::packet::{NodeType, Packet};
 use std::thread;
-use std::time::Duration;
-use dronegowski_utils::hosts::ServerType::Communication;
 use serde::de::DeserializeOwned;
 use crate::communication_server::CommunicationServer;
 
@@ -25,21 +19,10 @@ pub trait DronegowskiServer {
     fn update_graph(&mut self, path_trace: Vec<(NodeId, NodeType)>);
     fn compute_best_path(&self, target_client: NodeId) -> Option<Vec<NodeId>>;
     fn reconstruct_message<T: DeserializeOwned>(&mut self, key: u64) -> Result<T, Box<dyn std::error::Error>>;
-
 }
 
 
 fn main() {
-    // Creazione dei canali
-    let (sim_controller_send, sim_controller_recv) = unbounded::<ServerEvent>();
-    let (send_controller, controller_recv) = unbounded::<ServerCommand>();
-    let (packet_send, packet_recv) = unbounded::<Packet>();
-
-    // Mappa dei vicini (drone collegati)
-    let (neighbor_send, neighbor_recv) = unbounded();
-    let mut senders = HashMap::new();
-    senders.insert(2, neighbor_send); // Drone 2 come vicino
-
     // creazione del Server
     let mut communication_server = CommunicationServer::new(
         generate_unique_id() as NodeId
